e9846e776a4058f4de523cdf425ba033d5018500
Andrea Powell <andreavpowell@gmail.com>
2021-08-10 23:01:17 -0700

    added notes, html, and script to AboutMe

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# AboutMe
+# About Me
+
+## Synopsis
+This is a project that I made during my first week of Code 201: Foundations of Software Development at Code Fellows. I was tasked with creating a site that has the user answering questions based on my personality, likes, dislikes, and general facts about me.
+
+## Code Example
+(In Progress)  
+
+## Motivation
+(In Progress)
+
+## Installation
+(In Progress)
+
+## API Reference
+(In Progress)
+
+## Tests
+(In Progress)  
+-
+-
+-
+-
+-
+-
+-
+-
+
+### Andrea Powell  
+### License: MIT
---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let userName = prompt('Please Enter Your Name: ');
+console.log(userName);
+document.write('<h2>' + userName + '</h2>');
+return userName;
+
+
+let favoriteColor = prompt('Is my favorite color pink?');
+console.log(favoriteColor);
+if (userAnswer.toLowerCase() == 'yes'){
+  document.write('<h3>Oh. Maybe you should go...</h3>');
+} else {
+  document.write('<h3>Sick. You\'re on the right track.<h3>');
+}
+
+let favoriteActivity = prompt('Would I rather be outside instead of indoors?');
+console.log(favoriteAcivity);
+if (userAnswer.toLowerCase() == 'yes'){
+  document.write('<h3>Nice guess! You got lucky on that one.</h3>');
+} else {
+  document.write('<h3>Oh. You have quite a bit to learn about me.</h3>');
+}
+
+let hairColor = prompt('Do I have blonde hair?');
+console.log(hairColor);
+if (userAnswer.toLowerCase() == 'yes'){
+  document.write('<h3>Cool. You\'re paying attention!</h3>');
+} else {
+  document.write('<h3>Wow. I hope you study harder on your quizzes...</h3>');
+}
+
+let outsideInside = prompt('Would I rather be outside instead of indoors?');
+console.log(outsideInside);
+if (userAnswer.toLowerCase() == 'yes'){
+  document.write('<h3>Nice guess! You got lucky on that one.</h3>');
+} else {
+  document.write('<h3>Oh. You have quite a bit to learn about me.</h3>');
+}
+
+let favoriteFood = prompt('Is pizza my favorite food?');
+console.log(favoriteFood);
+if (userAnswer.toLowerCase() == 'yes'){
+  document.write('<h3>Yes. All food is my favorite food, ' + userName + '.<h3>');
+} else {
+  document.write('<h3>All food is my favorite. Equality, ' + userName + '.<h3>');
+}
+
+
+// Welcome alert with userName
+
+// Remo - why isnt my app.js linking?
